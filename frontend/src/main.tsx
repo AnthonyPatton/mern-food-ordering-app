83351360e398786782611f9100aa5ab53ac3a8e5
Anthony Patton <anthonyalanpatton@gmail.com>
2024-06-09 17:19:40 -0700

    Deleted a few files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import './global.css';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
